522e02845274430f26ad631484ea58ca3ca4a6a8
art1mis <malik0786sam@gmail.com>
2021-03-19 18:50:26 +0530

    Added .prettierrc

--- background.js
+++ background.js
@@ -1,62 +1,60 @@
 // Supported Formats List along with some sample videos
 const formats = [
-  ".m3u8",
-  ".mpd", // https://s3.amazonaws.com/_bc_dml/example-content/sintel_dash/sintel_vod.mpd
-  ".mp4", // https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4
-  ".webm", // https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-webm-file.webm
-  ".ogv", // https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-ogv-file.ogv
-  ".mov", // https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mov-file.mov
-  ".mp3",
+  '.m3u8',
+  '.mpd', // https://s3.amazonaws.com/_bc_dml/example-content/sintel_dash/sintel_vod.mpd
+  '.mp4', // https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4
+  '.webm', // https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-webm-file.webm
+  '.ogv', // https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-ogv-file.ogv
+  '.mov', // https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mov-file.mov
+  '.mp3'
 ];
 
 // State
 const state = {
-  enabled: true,
+  enabled: true
 };
 
 // Actions for mutating extension behaviour
 const setEnabled = (newState = true) => {
   state.enabled = newState;
   state.enabled
-    ? chrome.browserAction.setIcon({ path: "icons/icon128.png" })
-    : chrome.browserAction.setIcon({ path: "icons/icon128_disabled.png" });
+    ? chrome.browserAction.setIcon({ path: 'icons/icon128.png' })
+    : chrome.browserAction.setIcon({ path: 'icons/icon128_disabled.png' });
 };
 
 // Creating Options in contextMenus
 chrome.contextMenus.create({
-  contexts: ["browser_action"],
-  id: "enable-disable",
-  title: "Enable/Disable Extension",
-  type: "checkbox",
-  checked: state.enabled,
+  contexts: ['browser_action'],
+  id: 'enable-disable',
+  title: 'Enable/Disable Extension',
+  type: 'checkbox',
+  checked: state.enabled
 });
 
 // Handling click on contextMenus Items
 chrome.contextMenus.onClicked.addListener((item) => {
   const { menuItemId, checked } = item;
-  if (menuItemId === "enable-disable") {
+  if (menuItemId === 'enable-disable') {
     setEnabled(checked);
   }
 });
 
 // Listening to keyboard shortcuts commands specified in manifest.json
-chrome.commands.onCommand.addListener(function (command) {
-  if (command === "toggle-extension") {
+chrome.commands.onCommand.addListener((command) => {
+  if (command === 'toggle-extension') {
     setEnabled(!state.enabled);
   }
 });
 
 // Utility Function
 // Genearte URL/Path Pattern For Formats
-const extPatternPaths = (formats) => {
-  return formats.map((format) => `*://*/*${format}*`);
-};
+const extPatternPaths = (formats) => formats.map((format) => `*://*/*${format}*`);
 
 // Capturing URL Input
 chrome.webRequest.onBeforeRequest.addListener(
-  function (info) {
+  (info) => {
     const isFormatSupported = (url, formats) => {
-      const resourceURL = url.split("?")[0].split("#")[0];
+      const resourceURL = url.split('?')[0].split('#')[0];
       for (const format of formats) {
         if (resourceURL.endsWith(format)) {
           return true;
@@ -65,9 +63,8 @@ chrome.webRequest.onBeforeRequest.addListener(
       return false;
     };
     if (state.enabled && isFormatSupported(info.url, formats)) {
-      var playerUrl =
-        chrome.runtime.getURL("player/player.html") + "#" + info.url;
-      if (navigator.userAgent.toLowerCase().indexOf("firefox") > -1) {
+      const playerUrl = chrome.runtime.getURL('player/player.html') + '#' + info.url;
+      if (navigator.userAgent.toLowerCase().indexOf('firefox') > -1) {
         chrome.tabs.update(info.tabId, { url: playerUrl });
         return { cancel: true };
       } else {
@@ -75,19 +72,21 @@ chrome.webRequest.onBeforeRequest.addListener(
       }
     }
   },
-  { urls: extPatternPaths(formats), types: ["main_frame"] },
-  ["blocking"]
+  { urls: extPatternPaths(formats), types: ['main_frame'] },
+  ['blocking']
 );
 
 // Capturing Omnibox Input
-chrome.omnibox.onInputEntered.addListener(function (input) {
+chrome.omnibox.onInputEntered.addListener((input) => {
+  // This also enables artibary input of the formats that are not listed above
+
   // if extension is disabled using enabled property
   // then it will get enable again if omnibox onInputEntered is triggered
   if (!state.enabled) {
     setEnabled(true);
   }
-  var playerUrl = chrome.runtime.getURL("player/player.html") + "#" + input;
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+  const playerUrl = chrome.runtime.getURL('player/player.html') + '#' + input;
+  chrome.tabs.query({ currentWindow: true, active: true }, (tabs) => {
     chrome.tabs.update(tabs[0].id, { url: playerUrl });
   });
 });
--- player/player.js
+++ player/player.js
@@ -1,33 +1,33 @@
-(function(window, videojs, href) {
+(function (window, videojs, href) {
   // Disable Videojs Logs
-  videojs.log.level('off')
-  const options ={
+  videojs.log.level('off');
+  const options = {
     fill: true,
     controls: true,
     autoplay: true
-  }
-  var player = window.player = videojs('player',{
+  };
+  var player = (window.player = videojs('player', {
     ...options,
     plugins: {
       httpSourceSelector: {
         default: 'auto'
       }
     }
-  });
-  const url = href.split('#')[1]
-  player.src(url)
+  }));
+  const url = href.split('#')[1];
+  player.src(url);
   player.httpSourceSelector();
-  player.ready(()=>{
-      // Disable dash.js Logs
-    if(player.dash && player.dash.mediaPlayer){
-      player.dash.mediaPlayer.getDebug().setLogToBrowserConsole(false)
+  player.ready(() => {
+    // Disable dash.js Logs
+    if (player.dash && player.dash.mediaPlayer) {
+      player.dash.mediaPlayer.getDebug().setLogToBrowserConsole(false);
     }
     // Focus Player for hotkeys to work
-    player.el().focus()
+    player.el().focus();
     // initialize the hotkeys plugin
     player.hotkeys({
       skipInitialFocus: false,
-      enableInactiveFocus : true,
+      enableInactiveFocus: true,
       volumeStep: 0.1,
       enableMute: true,
       enableFullscreen: true,
@@ -37,24 +37,24 @@
       seekStep: 10,
 
       // Enhance existing simple hotkey with a complex hotkey
-      fullscreenKey: function(e) {
+      fullscreenKey: function (e) {
         // fullscreen with the F key or Ctrl+Enter
-        return ((e.which === 70) || (e.ctrlKey && e.which === 13));
+        return e.which === 70 || (e.ctrlKey && e.which === 13);
       },
       customKeys: {
         togglePlayKey: {
-          key: function(e){
-            return (e.key === 'k' || e.key === "K")
+          key: function (e) {
+            return e.key === 'k' || e.key === 'K';
           },
-          handler: function(player){
-            if(player.paused()){
-              player.play()
-            }else{
-              player.pause()
+          handler: function (player) {
+            if (player.paused()) {
+              player.play();
+            } else {
+              player.pause();
             }
           }
         }
       }
-    })
-  })
-}(window, window.videojs, window.location.href));
+    });
+  });
+})(window, window.videojs, window.location.href);
